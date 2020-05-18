9e9327b7eb2422da8df1c7aab15793f60c86ccbf
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-05-18 18:13:02 +0200

    add login form method

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import axios from 'axios';
 
 
-
 const LoginPage = () => {
 
     const [username, setUsername] = useState();
@@ -20,11 +19,36 @@ const LoginPage = () => {
         })
     };
 
+    const onLogin = () => {
+        let user = {
+            username: username,
+            password: password
+        };
+        axios.post("http://localhost:8080/auth/signin", user).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    };
+
     return (
-        <div className="loginForm">
-            <input onChange={(e)=>setUsername(e.target.value)}/>
-            <input onChange={(e)=>setPassword(e.target.value)}/>
-            <button onClick={onSubmit}>Submit</button>
+        <div>
+            <div className="registration">
+                <input
+                    onChange={(e) => setUsername(e.target.value)}/>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={onSubmit}>Submit</button>
+            </div>
+            <div className="loginForm">
+                <input
+                    onChange={(e) => setUsername(e.target.value)}/>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={onLogin}>Submit</button>
+            </div>
         </div>
     );
 };
