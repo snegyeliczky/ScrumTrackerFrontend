25e904f04865ef0d8fceecf065847fb1906b9485
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-05-19 12:28:03 +0200

    cors problem

--- frontend/src/components/mainPage/AddNewProject.js
+++ frontend/src/components/mainPage/AddNewProject.js
@@ -1,21 +1,27 @@
 import React, {useState} from "react";
 import axios from 'axios';
 
-const AddNewProject =()=>{
+const AddNewProject = () => {
 
     const [projectName, setProjectName] = useState("");
 
     async function addNewProject() {
         console.log(projectName);
-        let url =""+projectName;
-       // await axios.post(url);
-
+        if (projectName.length < 3) {
+            alert("Project name must be minimum 3 character")
+        } else {
+            let projectNameObject = {projectName: projectName};
+            let axiosResponse = await axios.post("http://localhost:8080/project/create", projectNameObject);
+            console.log(axiosResponse);
+        }
     }
 
-    return(
+    return (
         <div>
             Create new project:
-            <input onChange={(e)=>{setProjectName(e.target.value)}}/>
+            <input onChange={(e) => {
+                setProjectName(e.target.value)
+            }}/>
             <button onClick={addNewProject}>Create Project</button>
         </div>
     )
