8067b2d7710d1408ba44c3b8dd15526c3c8cea61
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-05-28 13:45:45 +0200

    delete left

--- frontend/src/components/projectPage/Column.js
+++ frontend/src/components/projectPage/Column.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Task from "./Task";
+import {
+    DeleteOutlined
+} from '@ant-design/icons';
 
 const Column = ({status, onDragEnter, dragItem, onDragEnd, deleteStatus}) => {
 
@@ -11,8 +14,7 @@ const Column = ({status, onDragEnter, dragItem, onDragEnd, deleteStatus}) => {
         <div className={"project_column"}
              onDragEnter={(e) => (onDragEnter(status.id))}
         >
-            <h3>{status.statusName}</h3>
-            <button onClick={handelDeleteClick}>Delete</button>
+            <div ><div className={"status_tool_container"}><DeleteOutlined onClick={handelDeleteClick}/></div><h3>{status.statusName} </h3></div>
             {status.tasks.length === 0 ?
                 <h4>No task yet</h4>
                 : status.tasks.map(task => {
