fbd1c81ac4d86c3e188f078a54c2a0fed3b82ec9
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-05-28 16:19:06 +0200

    fix typo

--- frontend/src/components/projectPage/Column.js
+++ frontend/src/components/projectPage/Column.js
@@ -6,15 +6,20 @@ import {
 
 const Column = ({status, onDragEnter, dragItem, onDragEnd, deleteStatus}) => {
 
-    const handelDeleteClick = () =>{
-      deleteStatus(status.id);
+    const handelDeleteClick = () => {
+        deleteStatus(status.id);
     };
 
     return (
         <div className={"project_column"}
              onDragEnter={(e) => (onDragEnter(status.id))}
         >
-            <div ><div className={"status_tool_container"}><DeleteOutlined onClick={handelDeleteClick}/></div><h3>{status.statusName} </h3></div>
+            <div>
+                <div className={"status_tool_container"}>
+                    <DeleteOutlined onClick={handelDeleteClick}/>
+                </div>
+                <h3>{status.statusName} </h3>
+            </div>
             {status.tasks.length === 0 ?
                 <h4>No task yet</h4>
                 : status.tasks.map(task => {
