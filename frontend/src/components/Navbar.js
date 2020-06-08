61efccab7a2f33d844a270527b2586caeecdc241
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-06-08 12:20:57 +0200

    save username to local storage, logaut redirect to login

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,16 +3,21 @@ import axios from "axios";
 import {Link} from "react-router-dom";
 import "../components/customCSS/NavStyle.css"
 import {ProjectContext} from "./contexts/ProjectContext";
+import {useHistory} from "react-router-dom";
 
 
 const Navbar = () => {
 
+    const history = useHistory();
     const {setProjects} = useContext(ProjectContext);
 
     async function handleLogout() {
         await axios.get("http://localhost:8080/auth/logout");
         setProjects([]);
+        localStorage.clear();
+
         alert("Logout successful");
+        history.push("/login");
     }
 
     return (
