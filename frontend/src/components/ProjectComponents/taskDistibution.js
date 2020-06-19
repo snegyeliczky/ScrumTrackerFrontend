9ee20b82310a2432befaad8acbdd3adce96e626a
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-06-19 15:38:31 +0200

    task distributin with icons its weekend time !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- frontend/src/components/ProjectComponents/taskDistibution.js
+++ frontend/src/components/ProjectComponents/taskDistibution.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { UserOutlined,SnippetsOutlined} from "@ant-design/icons";
 
 const TaskDistibution = ({user}) => {
     return (
         <div className={"task_distribution"}>
-            {user[0]} : {user[1]}
+            {user[0]} : {user[1]} <SnippetsOutlined />
         </div>
     );
 };
