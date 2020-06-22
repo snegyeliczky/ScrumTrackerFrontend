3c5a5dfd255f68ee5abca2360e3085f215cc0096
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-06-22 14:15:21 +0200

    show project count if add project increment count

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = () => {
                 <div className={"homepage_data_container"}>
                     <h1>WELCOME {localStorage.getItem("username")} !</h1>
                     <a href={"/projects"}>Let's check your projects <SnippetsOutlined /> </a>
-                    {user.projects}
+                    <p> Total projects <SnippetsOutlined /> :  {user.projectsCount} </p>
                 </div>
             </div>
         </div>
