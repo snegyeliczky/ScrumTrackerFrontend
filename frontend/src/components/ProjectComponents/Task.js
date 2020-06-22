106f5073069f5e04a98366732466f0ebc1a145b7
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-06-22 11:50:12 +0200

    if have owner have smiley face

--- frontend/src/components/ProjectComponents/Task.js
+++ frontend/src/components/ProjectComponents/Task.js
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useContext, useEffect} from 'react';
-import {DeleteOutlined, ApiOutlined, AlertTwoTone } from "@ant-design/icons";
+import {DeleteOutlined, ApiOutlined, AlertTwoTone,SmileTwoTone  } from "@ant-design/icons";
 import {ContentContainer} from "../../Assets/StyledComps/styled";
 import TaskModal from "../TaskModal";
 import TaskCalls from "../../Services/TaskCalls";
@@ -86,6 +86,7 @@ const Task = ({task, statusId, onDragEnd, dragItem, handleDeleteTask, refreshSta
             </div>
             <div className="project_title">{thisTask.title}</div>
             <div className={"task_data_container"}>
+                <div className={"owner"}>{thisTask.owner?<SmileTwoTone />:''}</div>
                 <div className={"businessValue"}>{thisTask.priority>0?"Pri.: "+thisTask.priority:''}</div>
                 <div className={"deadline"}>
                     {thisTask.deadline ? new Intl.DateTimeFormat('en-US', visualDateFormat).format(new Date(thisTask.deadline)) : ""}
