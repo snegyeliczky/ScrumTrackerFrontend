71683233f9fff980fe0dffe6ddbfddb9ab5aaf30
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-06-23 12:37:14 +0200

    hom page re designed

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,31 +1,46 @@
 import React, {useEffect, useState} from 'react';
-import {SnippetsOutlined} from "@ant-design/icons";
+import {SnippetsOutlined, BookOutlined} from "@ant-design/icons";
 import UserCalls from "../Services/UserCalls";
 import {use} from "@js-joda/core";
+import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined";
 
 const HomePage = () => {
 
-    let [user,setUser] = useState({});
+    let [user, setUser] = useState({});
 
-    const getUser=async ()=>{
-      let userObj = await UserCalls.getUser(localStorage.getItem("username"));
-      console.log(userObj);
-      setUser(userObj.data);
+    const getUser = async () => {
+        let userObj = await UserCalls.getUser(localStorage.getItem("username"));
+        console.log(userObj);
+        setUser(userObj.data);
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getUser();
-    },[]);
+    }, []);
 
     return (
         <div className={"homepage_container"}>
             <div className="homepage">
                 <div className={"homepage_data_container"}>
                     <h1>WELCOME {localStorage.getItem("username")} !</h1>
-                    <p> Total projects <SnippetsOutlined /> :  {user.projectsCount} </p>
-                    <p> Task have:  <SnippetsOutlined /> :  {user.tasksCount} </p>
-                    <p> Participated <SnippetsOutlined /> :  {user.participantCount} </p>
-                    <a href={"/projects"}>Let's check your projects <SnippetsOutlined /> </a>
+                    <a href={"/projects"}>Let's check your projects <BookOutlined/> </a>
+                </div>
+                <div className={"user_stats"}>
+                    <div className={"homepage_stat_container"}>
+                        <BookOutlined className={"icon"}/>
+                        <p> Created projects by you: </p>
+                        <p className={"stat_count"}>{user.projectsCount}</p>
+                    </div>
+                    <div className={"homepage_stat_container"}>
+                        <UserOutlined className={"icon"}/>
+                        <p>You Participated in projects:</p>
+                        <p className={"stat_count"}>{user.participantCount}</p>
+                    </div>
+                    <div className={"homepage_stat_container"}>
+                        <SnippetsOutlined className={"icon"}/>
+                        <p> Tasks in your ownership:</p>
+                        <p className={"stat_count"}>{user.tasksCount}</p>
+                    </div>
                 </div>
             </div>
         </div>
