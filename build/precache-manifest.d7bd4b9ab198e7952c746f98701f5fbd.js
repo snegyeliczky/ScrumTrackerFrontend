14faa7711570d5006e27da2c8e10189dd14ca606
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-07-03 13:34:25 +0200

    redirects added

---
+++ build/precache-manifest.d7bd4b9ab198e7952c746f98701f5fbd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3456efd395f2244ed483f670ab5fc389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a88ef38a039377413de",
+    "url": "/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "691bcc5472e08326185b",
+    "url": "/static/css/main.950b7e41.chunk.css"
+  },
+  {
+    "revision": "8a88ef38a039377413de",
+    "url": "/static/js/2.61939000.chunk.js"
+  },
+  {
+    "revision": "64bbe077566df0629e4da569db84ac89",
+    "url": "/static/js/2.61939000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691bcc5472e08326185b",
+    "url": "/static/js/main.e1bdaae4.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "08dd39d73ac9b17bd63fa2b065f55fa3",
+    "url": "/static/media/ideas-whiteboard-person-working-7369.08dd39d7.jpg"
+  },
+  {
+    "revision": "8ada40744e8eee57642ea06abd6d994c",
+    "url": "/static/media/man-wearing-black-and-white-stripe-shirt-looking-at-white-212286.8ada4074.jpg"
+  }
+]);
